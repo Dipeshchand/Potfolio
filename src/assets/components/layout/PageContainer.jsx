dcea877acd5584d4a0445ccb5a4a8f6ad1a0ad5b
Dipeshchand <dipeshchand972@gmail.com>
2026-09-03 10:00:30 +0530

    routing error updated

--- src/assets/components/layout/PageContainer.jsx
+++ src/assets/components/layout/PageContainer.jsx
@@ -1,6 +1,6 @@
 const PageContainer = ({children})=>{
     return(
-        <div className="mx-auto w-full max-w-[880px] border-x border-neutral-200">
+        <div className="mx-auto w-full max-w-[680px] border-x border-neutral-200">
             {children}
         </div>
     )
